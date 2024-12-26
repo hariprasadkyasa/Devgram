5b82479b6d74658ab5d533df4de1ba0de52c7a6c
Hari <hari@avarobotics.com>
2024-12-27 02:50:35 +0530

    XCode project setup

---
+++ Devgram/Devgram/DevgramApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevgramApp.swift
+//  Devgram
+//
+//  Created by Raghavendra Hariprasad Kyasa on 27/12/24.
+//
+
+import SwiftUI
+
+@main
+struct DevgramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
